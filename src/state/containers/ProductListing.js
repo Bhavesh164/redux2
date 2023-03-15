0cdf3d3ad709969cdb7cdb1b00cda05c82d067cf
bhavesh <bhaveshverma333@gmail.com>
2023-03-15 18:08:22 +0530

    axios api

--- src/state/containers/ProductListing.js
+++ src/state/containers/ProductListing.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React,{useEffect} from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
 const ProductLisiting = () => {
 	// in order to get the access of redux store
 	const  products = useSelector((state)=>state);
+	const fetchProducts = async() => {
+	const response = await axios
+	.get("https://fakestoreapi.com/products")
+	.catch((err) => {
+		console.log("error",err);
+	});
+	console.log(response);
+	};
 	console.log(products);
+	useEffect(()=>{
+		fetchProducts();
+	},[]);
 	return (
 	<div className="ui grid container">
 			<ProductComponent />
